c70ef7007a913a9a915988e620e00d94e5cad1ed
Gabor Cserto <csertog@gmail.com>
2020-07-09 12:58:44 +0200

    Updates

---
+++ precache-manifest.5d303379811921e3c7da8e0bf1a648b4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9be65f402a9d2b59c4a8ac700d6b4f53",
+    "url": "/weather-app-react/index.html"
+  },
+  {
+    "revision": "3507b8598bcc8874b4c4",
+    "url": "/weather-app-react/static/css/2.8169083c.chunk.css"
+  },
+  {
+    "revision": "810c43e7b9a9c6a175dc",
+    "url": "/weather-app-react/static/css/main.84bf5671.chunk.css"
+  },
+  {
+    "revision": "3507b8598bcc8874b4c4",
+    "url": "/weather-app-react/static/js/2.dd951b30.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/weather-app-react/static/js/2.dd951b30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "810c43e7b9a9c6a175dc",
+    "url": "/weather-app-react/static/js/main.01c0b7ba.chunk.js"
+  },
+  {
+    "revision": "0965184a592c71c34fe2",
+    "url": "/weather-app-react/static/js/runtime-main.0d456eba.js"
+  },
+  {
+    "revision": "00ee27ed865f22d77c7cd0c1f4290ab9",
+    "url": "/weather-app-react/static/media/owfont-regular.00ee27ed.svg"
+  },
+  {
+    "revision": "3d73387c2a1e21f207a8357724f536ba",
+    "url": "/weather-app-react/static/media/owfont-regular.3d73387c.ttf"
+  },
+  {
+    "revision": "e00c269cd4fb37b24eea2e4f37982c3d",
+    "url": "/weather-app-react/static/media/owfont-regular.e00c269c.eot"
+  },
+  {
+    "revision": "ed668bf0b123371be46d005b428f6188",
+    "url": "/weather-app-react/static/media/owfont-regular.ed668bf0.woff"
+  }
+]);

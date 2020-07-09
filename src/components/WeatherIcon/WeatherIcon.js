0c9574ec9e127a8a4779e3638f4100e950818a63
Gabor Cserto <csertog@gmail.com>
2020-07-10 00:22:02 +0200

    Refactored weather icons

--- src/components/WeatherIcon/WeatherIcon.js
+++ src/components/WeatherIcon/WeatherIcon.js
@@ -2,13 +2,9 @@ import React from 'react';
 import * as WeatherIcons from "react-icons/wi";
 
 const weatherIcon = props => {
-	let weatherType;
+	let weatherType = <WeatherIcons.WiNa/>;
 
-	if (props.type) {
-		weatherType = <i className={`owf owf-3x owf-${props.type}-d`} />;
-	} else {
-		weatherType = <WeatherIcons.WiAlien/>;
-	}
+	if (props.type) weatherType = <i className={`owf owf-3x owf-${props.type}-d`} />;
 
 	return <div className="weatherIcon">{weatherType}</div>;
 }
